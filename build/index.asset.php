594038cfe1cc5a5df2573df2c055efe2b308905a
Axiel Roque <axiel.roque@outlook.es>
2026-08-20 11:09:36 -0400

    build: rebuild bundle to include snapshot disclosure UI (missing since PR #10)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element'), 'version' => '261e2d6b16d062ce1b3b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element'), 'version' => '741864e08ab8e1e8b091');
