89a04ef7f63232dd061f797411ab182952af4712
Axiel Roque <axiel.roque@outlook.es>
2026-08-20 12:58:10 -0400

    build: rebuild bundle for the PROFITLENS_DEV banner label update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element'), 'version' => '741864e08ab8e1e8b091');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element'), 'version' => '1b921b7f62bb41d71571');
